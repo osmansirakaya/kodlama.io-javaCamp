fdfdc39a0e93b9e36e5466528239e9f128650f37
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-10-06 21:16:32 +0300

    classes

---
+++ classes/src/CustomerManager.java
@@ -0,0 +1,13 @@
+public class CustomerManager { // müşteri ile ilgili neler yapılacaksa onları yazarız bu class'a
+    public void Add(){
+        System.out.println("Müşteri eklendi");
+    }
+
+    public void Remove(){
+        System.out.println("Müşteri silindi");
+    }
+
+    public void Update(){
+        System.out.println("Müşteri güncellendi");
+    }
+}
