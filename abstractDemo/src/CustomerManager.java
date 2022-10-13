3e163f924d3940b0e5836f4dd02249998e114a09
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-10-14 00:12:31 +0300

    abstract

---
+++ abstractDemo/src/CustomerManager.java
@@ -0,0 +1,6 @@
+public class CustomerManager {
+    BaseDatabaseManager databaseManager; //databaseManager kullanarak
+    public void getCustomer(){
+        databaseManager.getData();
+    }
+}
---
+++ abstractDemo/src/OracleDatabaseManager.java
@@ -0,0 +1,6 @@
+public class OracleDatabaseManager  extends BaseDatabaseManager{
+    @Override
+    public void getData() {
+        System.out.println("Data has been got from Oracle.");
+    }
+}
