ff97d208c5cbe637d2c1d41ff42d9e5c662a8aa5
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-10-17 19:58:21 +0300

    interfaceDemo

---
+++ interfaceDemo/src/IEetable.java
@@ -0,0 +1,3 @@
+public interface IEetable {
+    void eat();
+}
---
+++ interfaceDemo/src/Worker.java
@@ -0,0 +1,14 @@
+// bizim çalışanımızın özelliklerini tutan sınıf
+public class Worker implements IWorkable, IPayable, IEetable {
+    @Override
+    public void work() {
+
+    }
+
+    public void pay() {
+
+    }
+    public void eat() {
+
+    }
+}
