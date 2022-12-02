f743b70601b9f65fe98d1537d54aca0684b56501
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-12-02 22:36:19 +0300

    readingFile

---
+++ readingFileDemo/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader reader = null;
+        try{
+            reader = new BufferedReader(new FileReader("C:\\btk-java\\readingFileDemo\\src\\sayilar.txt"));
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            }catch (Exception exception){
+
+            }
+        }
+
+    }
+}
