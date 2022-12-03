c6e80fb9b33e9f8e5feee4afa99b7d2035686f3c
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-12-03 20:21:25 +0300

    customException

---
+++ throwDemo/src/AccountManager.java
@@ -0,0 +1,17 @@
+public class AccountManager {
+    private double balance;
+
+    public void deposit(double amount){
+        balance = getBalance() + amount;
+    }
+    public void withdraw(double amount) throws BalanceInsufficentException {
+        if (balance>=amount){
+            balance = getBalance() - amount;
+        }else {
+            throw new BalanceInsufficentException("bakiye yetersiz.");
+        }
+    }
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ throwDemo/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+       AccountManager manager = new AccountManager();
+       manager.deposit(100);
+       System.out.println(manager.getBalance());
+       try{
+           manager.withdraw(90);
+       }catch (BalanceInsufficentException exception){
+           System.out.println(exception.getMessage());
+       }
+       System.out.println(manager.getBalance());
+        try{
+            manager.withdraw(20);
+        }catch (BalanceInsufficentException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+}
