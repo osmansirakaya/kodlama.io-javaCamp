4a5091433d5488bfdd871e5918db5c7e1f775789
Osman SIRAKAYA <osmansirakaya96@gmail.com>
2022-12-08 22:38:12 +0300

    generic

---
+++ generics/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Mylist<Customer> sehirler = new Mylist<Customer>();
+        sehirler.add(new Customer());
+    }
+}
---
+++ generics/src/Mylist.java
@@ -0,0 +1,8 @@
+public class Mylist <T>{ // type'nin T'si
+    public void add(T value){
+
+    }
+    public void remove(T value){
+
+    }
+}
